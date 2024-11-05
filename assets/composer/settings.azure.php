8aed963a2fd7d49b5a68b899b2e4b0ac8d57c0e5
Stephen Cox <stephen@agile.coop>
2024-11-05 16:40:37 +0000

    Updates for Azure file system mount

--- assets/composer/settings.azure.php
+++ assets/composer/settings.azure.php
@@ -12,4 +12,10 @@ $databases['default']['default'] = [
   'driver' => 'mysql',
   'prefix' => '',
   'collation' => 'utf8mb4_general_ci',
+  'pdo' => [
+    \PDO::MYSQL_ATTR_SSL_CA   => '/etc/ssl/certs/DigiCert_Global_Root_CA.pem',
+    \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => FALSE,
+  ],
 ];
+
+$settings['file_private_path'] = '/var/www/shared/private_files';
